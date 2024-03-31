0eeee35b1a5faf02d4985fb91bc70e7c48300ef2
ManishKashyap-2024 <159691388+ManishKashyap-2024@users.noreply.github.com>
2024-03-31 11:26:41 +0800

    Update stock_price.py

--- stock_price.py
+++ stock_price.py
@@ -9,7 +9,7 @@ import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 
-with open('../cred.yaml') as file:
+with open('IA_project/cred.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = stauth.Authenticate(
