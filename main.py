7ebd31f0d692db3a76a282c0de95668be38115f5
Waqar Ul Wahab <waqar.wahab412@gmail.com>
2024-08-30 00:18:46 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ def main():
 
 
 
+    st.markdown("""
+    <style>
+        [data-testid=stSidebar] {
+            background-color: #3e3e40;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+    with st.sidebar:
+            st.image("./Animations/stock.png", 
+                    #caption="Your PNG caption",
+                    width=350)                
+
+    #st.sidebar.title("hAin")           
+    st.sidebar.markdown("<hr>", unsafe_allow_html=True)
+
+
     st.sidebar.page_link("main.py",           label="Login",    icon="🔐")
     st.sidebar.page_link("pages/register.py", label="Register", icon="👤")
     st.sidebar.page_link("pages/forgot.py",   label="Forgot Credential",   icon="❓")
